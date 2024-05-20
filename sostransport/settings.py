69617343fc511c675602fe53e75089051bdb2909
Moustaphalkarim <33578614+moustapha-blsq@users.noreply.github.com>
2024-05-20 01:14:52 +0000

    staticfiles

--- sostransport/settings.py
+++ sostransport/settings.py
@@ -144,8 +144,9 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'), )
 
 django_heroku.settings(locals())
